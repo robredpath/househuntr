4615cf2ae81b203812db33fa746025c651de3005
Rob Redpath <rob@digital-saints.com>
2015-10-10 22:38:25 +0100

    update

--- mvp.rb
+++ mvp.rb
@@ -27,18 +27,18 @@ post '/' do
 	# is there an 'x distance from a tram stop' type query? 
 	# MOCKUP - let's assume yes, and let's assume it's just the one tram stop and 3 mins walk
 
-	#from_tram_stop = "Nottingham%20Trent%20University%20Tram%20Stop"
+	from_tram_stop = "Nottingham%20Trent%20University%20Tram%20Stop"
 
-	#simple_search_data.each do |property|
-	#	uri = URI("https://maps.googleapis.com/maps/api/distancematrix/json?mode=walking&origins=#{property[1]}&destinations=#{from_tram_stop}&key=AIzaSyBz0ZEOpH17m35flnCwMrkei1xHlWgZohQ")
-#
-#		Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|   
-#			request = Net::HTTP::Get.new uri
-#			response = http.request request # Net::HTTPResponse object 
-#			duration = JSON.parse(response.body)["rows"].first["elements"].first["duration"]["text"] 
-#			output.concat "#{property[2]} in #{property[1]} for £#{property[0]} and #{duration} mins walk from trent tram stop<br />"
-#		end 
-#	end
+	simple_search_data.each do |property|
+		uri = URI("https://maps.googleapis.com/maps/api/distancematrix/json?mode=walking&origins=#{property[1]}&destinations=#{from_tram_stop}&key=AIzaSyBz0ZEOpH17m35flnCwMrkei1xHlWgZohQ")
+
+		Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|   
+			request = Net::HTTP::Get.new uri
+			response = http.request request # Net::HTTPResponse object 
+			duration = JSON.parse(response.body)["rows"].first["elements"].first["duration"]["text"] 
+			output.concat "#{property[2]} in #{property[1]} for £#{property[0]} and #{duration} mins walk from trent tram stop<br />"
+		end 
+	end
 
 	# if so => grab additional data from google maps API
 	# is there an 'x distance from the motorway' type query?
@@ -48,10 +48,9 @@ post '/' do
 	# filter results according to google maps data
 	
 	# build up the new filter list
-	#filters = params.select { |k,v| /^filter/.match(k) }
+	filters = params.select { |k,v| /^filter/.match(k) }
 
- 	params.inspect
-
+	filters 
 	# present results
 	# => If there's no existing request, give the empty page
 	# => if there is an existing request, display it with the ability to add another item the query
